1627e312dae9fb33098febd1b65cdea5ed3b3102
Yuki Takei <yuki@weseek.co.jp>
2021-02-19 09:54:12 +0000

    fix settings for typescript

--- typescript-next.js
+++ typescript-next.js
@@ -5,16 +5,6 @@ module.exports = {
     './react',
     './typescript',
   ],
-  settings: {
-    // prevent import/no-unresolved
-    // see: https://qiita.com/282Haniwa/items/76d56a6a7e9d0db95a33
-    'import/resolver': {
-      typescript: {
-        config: path.join(__dirname, './webpack.config.js'),
-        alwaysTryTypes: true,
-      },
-    },
-  },
   rules: {
     // It's not required since Next.js will automatically add `import React from 'react'`
     'react/react-in-jsx-scope': 'off',
--- typescript.js
+++ typescript.js
@@ -15,6 +15,16 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
   ],
+  settings: {
+    // prevent import/no-unresolved
+    // see: https://qiita.com/282Haniwa/items/76d56a6a7e9d0db95a33
+    'import/resolver': {
+      typescript: {
+        config: path.join(__dirname, './webpack.config.js'),
+        alwaysTryTypes: true,
+      },
+    },
+  },
   rules: {
     // respect eslint-config-airbnb
     // see: https://github.com/airbnb/javascript/blob/5b462a04cdfebba712a3df18926edfcb4103d2a9/packages/eslint-config-airbnb-base/rules/best-practices.js#L88-L94
