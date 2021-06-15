b4030d0ea7fac5c37f2d1586296df55a9d4a3244
atsuki-t <atsuki-t@weseek.co.jp>
2021-06-15 20:43:11 +0900

    fix import rule

--- vue.js
+++ vue.js
@@ -3,6 +3,8 @@ module.exports = {
     'plugin:vue/recommended'
   ],
   rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
     'vue/html-self-closing': 'off',
     'vue/max-attributes-per-line': 'off',
     'vue/singleline-html-element-content-newline': 'off'
