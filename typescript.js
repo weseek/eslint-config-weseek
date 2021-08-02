4c683954aa06a58061f939b8f62a96ecba50fbff
Yuki Takei <yuki@weseek.co.jp>
2021-08-02 12:45:18 +0000

    disable @typescript-eslint/no-unused-vars in js

--- typescript.js
+++ typescript.js
@@ -37,6 +37,7 @@ module.exports = {
       files: ['*.js', '*.jsx'],
       rules: {
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
     {
